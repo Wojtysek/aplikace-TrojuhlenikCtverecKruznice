cdca8226b230d0af1b4aa1904a50775a9b2b9189
Vojtech Hruby <vojtech.hruby.020@pslib.cz>
2021-11-07 15:57:46 +0100

    dokonceno

--- aplikace-TrojuhlenikCtverecKruznice.cpp
+++ aplikace-TrojuhlenikCtverecKruznice.cpp
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(void) {
-	int vstup, a, b, c, r;
+	int vstup, a, b, c, r, o;
 
 	printf("vloz operaci: \n");
 	printf("1) trojuhelnik\n");
@@ -23,11 +24,27 @@ int main(void) {
 		if (a + b > c && a + c > b && c + b > a) {
 			printf("tento trojuhelnik existuje\n");
 			int max = a;
+			int k = b, l = c;
 			if (b > max) {
 				max = b;
+				k = a;
+				l = c;
 			}
 			if (c > max) {
 				max = c;
+				k = a;
+				l = b;
+			}
+			if (max * max == k * k + l * l) {
+				int ava = k * l;
+				printf("tento trojuhelnik je pravouhly\n");
+				printf("obsah: %d\n", ava / 2);
+			}
+			else {
+				printf("tento trojuhelnik neni pravouhly\n");
+				o =((a + b + c) / 2);
+				printf("obsah: %d\n", sqrt(o * (o - a) * (o - b) * (o - c)));
+				printf("zkouska: %f\n", o);
 			}
 			printf("obvod: %d\n", a + b + c);
 		}
@@ -49,15 +66,15 @@ int main(void) {
 		else {
 			printf("je to obdelnik\n");
 		}
-		printf("obvod: %d\n", 2 * a + 2 * b);
 		printf("obsah: %d\n", a * b);
+		printf("obvod: %d\n", 2 * a * 2 * b);
 	}
 	else {
 			//kruznice
 		printf("zadej polomer kruznice: ");
 		scanf("%d", &r);
 		system("cls");
-		printf("obvod: %f\n", 3.14 * 2 * r);
 		printf("obsah: %f\n", 3.14 * r * r);
+		printf("obvod: %f\n", 3.14 * 2 * r);
 	}
 }
